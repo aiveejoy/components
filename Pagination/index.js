86b0781a51290afd9aa77336e09da30119031f08
Kennette Canales <kennettecanales@gmail.com>
2020-07-13 12:13:09 +0800

    update

--- Pagination/index.js
+++ Pagination/index.js
@@ -1,23 +1,15 @@
 import React, {Component} from 'react';
 import styles from './Style';
 import {Text, View, TouchableOpacity, ScrollView, Dimensions} from 'react-native';
-import { Pager, PagerProvider } from '@crowdlinker/react-native-pager';
 import {BasicStyles, Color, Helper} from 'common';
 const width = Math.round(Dimensions.get('window').width);
 const height = Math.round(Dimensions.get('window').height);
 class Pagination extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      activeIndex: 0
-    };
-  }
-  onChange(e){
-    console.log('on change', e)
   }
 
   render(){
-    const { activeIndex } = this.state;
     return (
       <View>
         <View style={{ height: 50, marginBottom: 10, backgroundColor: Color.primay }}>
@@ -25,41 +17,23 @@ class Pagination extends Component {
             {
               Helper.pagerMenu.map((item, index) => (
                 <TouchableOpacity
-                  onPress={() => this.setState({
-                    activeIndex: index
-                  })}
+                  onPress={() => this.props.onChange(index)}
                   style={{
                     width: width / 3,
                     borderBottomWidth: 2,
-                    borderBottomColor: activeIndex == index ? Color.primary : Color.lightGray,
+                    borderBottomColor: this.props.activeIndex == index ? Color.primary : Color.lightGray,
                     justifyContent: 'center',
                     alignItems: 'center'
                   }}>
                   <Text style={{
                     fontSize: 11,
-                    color: activeIndex == index ? Color.primary : Color.black
+                    color: this.props.activeIndex == index ? Color.primary : Color.black
                   }}>{item.title}</Text>
                 </TouchableOpacity>
               ))
             }
           </ScrollView>
         </View>
-        <PagerProvider activeIndex={activeIndex}>
-          <Pager>
-            <View style={styles.MainContainer}>
-              <Text>Featured</Text>
-            </View>
-            <View style={styles.MainContainer}>
-              <Text>Categories</Text>
-            </View>
-            <View style={styles.MainContainer}>
-              <Text>Shops</Text>
-            </View>
-            <View style={styles.MainContainer}>
-              <Text>Others</Text>
-            </View>
-          </Pager>
-        </PagerProvider>
       </View>
     );
   }
