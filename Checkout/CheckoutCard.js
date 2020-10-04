b59dd5c5c5911876607db20926efad797fe62bf6
troy1129 <44769850+troy1129@users.noreply.github.com>
2020-10-04 13:49:30 +0800

    Update

--- Checkout/CheckoutCard.js
+++ Checkout/CheckoutCard.js
@@ -3,7 +3,7 @@ import { Text, View, Image, TouchableOpacity } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import Style from './checkoutCardStyle';
-import {faUserCircle,faMapMarker, faUniversity,faKaaba,faPlus,faMinus} from '@fortawesome/free-solid-svg-icons';
+import {faUserCircle,faMapMarker, faUniversity,faKaaba,faPlus,faMinus,faImage} from '@fortawesome/free-solid-svg-icons';
 import Config from 'src/config.js'
 
 class CheckoutCard extends Component {
@@ -14,18 +14,32 @@ class CheckoutCard extends Component {
   render() {
     const { details } = this.props
     const {index}=this.props;
+    const ImageDisplay = (
+      details.featured == null 
+      ? <FontAwesomeIcon icon={faImage} size={100} style={[Style.image, {color: '#ccc'}]} />
+      : <Image source={{ uri: Config.BACKEND_URL + details.featured[0].url }} style={Style.image} />
+    )
     return (
       <View style={Style.container}>
    
         <View style={Style.details}>
+          {details.price!=null? 
+          (
+            <React.Fragment>
           <View style={Style.newLeft}>
-          <TouchableOpacity onPress={()=>this.props.onSubtract()}><FontAwesomeIcon style={{paddingRight:10}} icon={faMinus} color={'orange'}/></TouchableOpacity>
-    <Text style={{fontSize:20,marginTop:-5,marginRight:5,marginLeft:5}}>{details.quantity}</Text>
-    <TouchableOpacity onPress={()=>this.props.onAdd()}><FontAwesomeIcon style={{paddingRight:10}} icon={faPlus} color={'orange'}/></TouchableOpacity>
+          <TouchableOpacity onPress={()=>this.props.onSubtract()}>
+            <FontAwesomeIcon style={{paddingRight:10}} icon={faMinus} color={'orange'}/>
+          </TouchableOpacity>
+             <Text style={{fontSize:20,marginTop:-5,marginRight:5,marginLeft:5}}>
+                {details.quantity}
+              </Text>
+            <TouchableOpacity onPress={()=>this.props.onAdd()}>
+               <FontAwesomeIcon style={{paddingRight:10}} icon={faPlus} color={'orange'}/>
+            </TouchableOpacity>
           </View>
           <View style={Style.leftSide}>
             <Text style={Style.title}>
-              { details.title}
+              {details.title}
             </Text>
             <Text style={Style.tags,{fontiSize:15}}>
              {`PHP `+(details.price[0].price*details.quantity)}
@@ -34,6 +48,24 @@ class CheckoutCard extends Component {
           <View style={Style.rightSide}>
           <Image source={{ uri: Config.BACKEND_URL+ details.featured[0].url }} style={Style.image} />
           </View>
+          </React.Fragment>
+          ) 
+          :
+           <React.Fragment>
+            <View style={Style.leftSide}>
+            <Text style={Style.title}>
+              {details.title}
+            </Text>
+            <Text>
+               This Item has No Price and was removed from your Cart.
+             </Text>
+          </View>
+          <View style={Style.rightSide}>
+          {ImageDisplay}
+          </View>
+             
+           </React.Fragment>
+           }
         </View>
       </View>
     );
