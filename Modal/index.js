ee15d8422e2ef2b0ced53132ca83d5fba4760bf0
yol <yoltorres2@gmail.com>
2020-11-09 08:28:19 +0800

    added QRCode Modal

--- Modal/index.js
+++ Modal/index.js
@@ -92,6 +92,7 @@ class CustomModal extends Component {
   }
 
   _charges = () => {
+    debugger
     const { data } = this.props;
     const { userLedger } = this.props.state;
     const { errorMessage } = this.state;
