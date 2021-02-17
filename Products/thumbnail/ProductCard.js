34e58e8fef1e1c94f61417caec2af9621327bf71
GarridoL <lalaine.garrido@student.passerellesnumeriques.org>
2021-02-17 20:01:11 +0800

    updates

--- Products/thumbnail/ProductCard.js
+++ Products/thumbnail/ProductCard.js
@@ -27,7 +27,6 @@ class ProductCard extends Component {
 
   render() {
     const { item, theme } = this.props;
-    console.log(item, "========");
     return (
       <TouchableOpacity
         style={item.batch_number && item.batch_number.length > 0 ? Style.selectedContainer : Style.cardContainer}
