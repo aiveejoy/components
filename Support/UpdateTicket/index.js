6a031b61b2c6a75b57223f86eee44180b8e62aa6
Kennette Canales <kennettecanales@gmail.com>
2021-07-13 14:05:03 +0800

    update

--- Support/UpdateTicket/index.js
+++ Support/UpdateTicket/index.js
@@ -6,7 +6,7 @@ import Api from 'services/api/index.js';
 import { Routes, BasicStyles } from 'common';
 import ImagePicker from 'react-native-image-picker';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faImages, faPaperPlane, faClock, faCaretDown, faEllipsisH } from '@fortawesome/free-solid-svg-icons';
+import { faImages, faPaperPlane, faClock, faCaretDown, faEllipsisH, faBug } from '@fortawesome/free-solid-svg-icons';
 import Color from 'common/Color';
 import { Dimensions } from 'react-native';
 import { connect } from 'react-redux';
@@ -23,22 +23,9 @@ class UpdateTicket extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: null,
-      description: null,
-      type: null,
-      id: null,
-      assignee: null,
-      status: null,
       isLoading: false,
-      images: [],
-      comment: null,
-      comments: [],
-      showComments: false,
-      reply: null,
-      date: null,
       modalVisible: false,
-      image: null,
-      closeTicket: false
+      data: null
     };
   }
 
@@ -58,17 +45,17 @@ class UpdateTicket extends Component {
     Api.request(Routes.ticketsRetrieve, parameter, response => {
       this.setState({ isLoading: false })
       if (response.data.length > 0) {
-        this.setState({
-          title: response.data[0].title,
-          description: response.data[0].content,
-          type: response.data[0].type,
-          id: response.data[0].id,
-          images: response.data[0].images ? response.data[0].images.split(' ') : [],
-          date: response.data[0].created_at_human,
-          status: response.data[0].status
+        console.log({
+          data: response.data[0]
         })
-        this.retrieveComments(this.props.navigation.state.params.id);
+        this.setState({
+          data: response.data[0]
+        })
       }
+    }, error => {
+      this.setState({
+        isLoading: false
+      })
     })
   }
 
@@ -175,21 +162,6 @@ class UpdateTicket extends Component {
     })
   }
 
-  choosePhoto = () => {
-    const options = {
-      noData: false,
-    }
-    ImagePicker.launchImageLibrary(options, response => {
-      if (response.uri) {
-        let images = this.state.images;
-        images.push(`data:image/png;base64,${response.data}`);
-        this.setState({ images: images });
-      } else {
-        this.setState({ photo: null });
-      }
-    })
-  }
-
   renderComments = () => {
     const { theme } = this.props.state;
     return (
@@ -256,131 +228,70 @@ class UpdateTicket extends Component {
     )
   }
 
-  render() {
+  renderTicket(){
+    const { data } = this.state;
     const { theme } = this.props.state;
-    let data = [{ title: 'Bug', value: 'bug' }, { title: 'Question', value: 'question' }, { title: 'Enhancement', value: 'enhancement' }, { title: 'Invalid', value: 'invalid' }, { title: 'Duplicate', value: 'duplicate' }, { title: 'Help wanted', value: 'help wanted' }]
-    let status = [{ title: 'Pending', value: 'pending' }, { title: 'Open', value: 'open' }, { title: 'Closed', value: 'closed' }]
-    console.log(this.state.date && this.state.date);
-    return (
-      <View style={styles.CreateTicketContainer}>
-        <ScrollView showsVerticalScrollIndicator={false}>
-        {this.state.isLoading && (<Skeleton size={0} template={'block'} height={50}/>)}
-          {this.state.title && (<View style={{
-            borderColor: Color.gray,
-            borderBottomWidth: .3,
-            paddingBottom: 20
+    return(
+      <ScrollView showsVerticalScrollIndicator={false}>  
+        <View style={{
+          borderColor: Color.gray,
+          borderBottomWidth: .3,
+          padding: 20
+        }}>
+          <View style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginBottom: 20
           }}>
-            <View style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              marginBottom: 20
-            }}>
-              <FontAwesomeIcon
-                icon={faClock}
-                size={25}
-                style={{
-                  marginRight: 5,
-                  color: theme ? theme.primary : Color.primary
-                }}
-              />
-              <Text>{this.state.date && this.state.date}</Text>
-              {this.state.status !== 'closed' ? <TouchableOpacity style={{
-                position: 'absolute',
-                right: 10
+            <FontAwesomeIcon
+              icon={faBug}
+              size={25}
+              style={{
+                marginRight: 5,
+                color: theme ? theme.primary : Color.primary
               }}
-                onPress={() => { this.setState({ closeTicket: !this.state.closeTicket }) }}
-              >
-                <FontAwesomeIcon
-                  icon={faEllipsisH}
-                  size={25}
-                  style={{
-                    color: theme ? theme.primary : Color.primary
-                  }}
-                />
-              </TouchableOpacity> :
-                <Text style={{
-                  position: 'absolute',
-                  right: 10,
-                  color: 'red'
-                }}>Closed</Text>
-              }
-              {this.state.closeTicket === true && (<TouchableOpacity style={{
-                position: 'absolute',
-                right: 10,
-                backgroundColor: 'white',
-                top: 30,
-                height: 45,
-                width: 105,
-                borderWidth: 1,
-                borderRadius: 10,
-                borderColor: Color.gray,
-                padding: 10,
-                zIndex: 10
-              }}
-                onPress={() => { this.update() }}>
-                <Text>Close Ticket</Text>
-              </TouchableOpacity>)}
-            </View>
+            />
             <View>
-              <Text style={{ fontWeight: 'bold', textDecorationLine: 'underline' }}>{this.state.title?.toUpperCase()}</Text>
-            </View>
-            <View style={{ marginTop: 20 }}>
-              <Text>{this.state.description && this.state.description}</Text>
-            </View>
-            <Text style={{
-              fontStyle: 'italic',
-              color: theme ? theme.primary : Color.primary,
-              marginTop: 30,
-              fontWeight: 'bold',
-              marginBottom: 10
-            }}>Attachment(s)</Text>
-            <View style={{ flexDirection: 'row', width: '90%' }}>
-              <ScrollView horizontal={true}>
-                {this.state.images.length > 0 && this.state.images.map((u, i) => {
-                  return (
-                    <View key={i} style={{ flexDirection: 'row' }}>
-                      <TouchableOpacity
-                        onPress={() => {
-                          this.setState({
-                            image: u,
-                            modalVisible: true
-                          })
-                        }}
-                      >
-                        <Image
-                          source={{ uri: u }}
-                          style={styles.Image}
-                        />
-                      </TouchableOpacity>
-                    </View>
-                  )
-                })}
-              </ScrollView>
-            </View>
-            <View style={{ flexDirection: 'row', marginTop: 30, }}>
               <Text style={{
-                fontWeight: 'bold',
-              }}>STATUS: </Text>
+              }}>{data.type}</Text>
               <Text style={{
-                color: this.state.status && this.state.status.toLowerCase() === 'closed' ? Color.danger : Color.success
-              }}>{this.state.status}</Text>
+                fontSize: 11
+              }}>{data.created_at_human}</Text>
             </View>
           </View>
-          )}
-          {this.renderComments()}
-          {/* <View style={styles.TicketButtonContainer}>
-            <TicketButton
-              buttonColor={Color.danger}
-              buttonWidth="90%"
-              buttonHeight={50}
-              fontSize={14}
-              textColor="#FFFFFF"
-              buttonText="Close Ticket"
-              onPress={this.update.bind(this)}
-            />
-          </View> */}
-          <ImageModal visible={this.state.modalVisible} url={this.state.image} action={() => { this.setState({ modalVisible: false }) }}></ImageModal>
-        </ScrollView>
+          <Text style={{ fontWeight: 'bold' }}>{data.title}</Text>
+          <Text style={{
+            paddingTop: 20,
+            paddingBottom: 20,
+            fontStyle: 'italic'
+          }}>{data.content}</Text>
+          
+          <View style={{ flexDirection: 'row'}}>
+            <Text style={{
+              fontWeight: 'bold',
+            }}>STATUS: </Text>
+            <Text style={{
+              color: data.status.toLowerCase() === 'closed' ? Color.danger : Color.success
+            }}>{data.status}</Text>
+          </View>
+        </View>
+        {data.comments && this.renderComments()}
+      </ScrollView>
+    )
+  }
+  render() {
+    const { theme } = this.props.state;
+    const { data, isLoading} = this.state;
+    return (
+      <View style={{
+        flex: 1
+      }}>
+        {isLoading && (<Skeleton size={3} template={'block'} height={50}/>)}
+        {
+          data && (
+            this.renderTicket()
+          )
+        }
       </View>
     );
   }
