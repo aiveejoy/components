4646d8739299717e292e6907e2cec0bba19b6d21
GarridoL <lalaine.garrido@student.passerellesnumeriques.org>
2022-01-04 11:14:03 +0800

    updates

--- Comments/Create.js
+++ Comments/Create.js
@@ -39,8 +39,8 @@ class Create extends Component {
     const { user } = this.props.state;
     let parameter = {
       account_id: user.id,
-      payload: this.props.pageId ? 'page_id' : 'account_id',
-      payload_value: this.props.pageId ? this.props.pageId : user.id,
+      payload: this.props.payload ? 'page' : 'account_id',
+      payload_value: this.props.payload ? this.props.payload?.payload_value : user.id,
       text: this.state.status,
       to: user.id,
       from: user.id,
