a222875886a861c4c522d84afec38970de1304c7
GarridoL <lalaine.garrido@student.passerellesnumeriques.org>
2022-02-10 10:04:20 +0800

    updates

--- Comments/Style.js
+++ Comments/Style.js
@@ -12,5 +12,22 @@ export default {
   },
   footerIcon: {
     marginTop: Platform.OS == 'ios' ? 30 : 0
+  },
+  option: {
+    marginTop: 5,
+    marginBottom: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: Color.lightGray
+  },
+  optionContainer: {
+    position: 'absolute',
+    right: 10,
+    top: 60,
+    zIndex: 1,
+    backgroundColor: Color.white,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: Color.lightGray,
+    borderRadius: 10
   }
 }
