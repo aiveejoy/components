471f0d6ea24b81ef7438592ffd7ea26f11b4c08d
GarridoL <lalaine.garrido@student.passerellesnumeriques.org>
2022-03-10 09:51:16 +0800

    updates

---
+++ Payments/GooglePay/index.js
@@ -0,0 +1,165 @@
+import React, { Component } from 'react';
+import { View, Dimensions, Text, Alert, ScrollView, Image } from 'react-native';
+import { connect } from 'react-redux';
+import Button from 'components/Form/Button';
+import Api from 'services/api/index.js';
+import { Color, Routes } from 'common';
+import { Spinner } from 'components';
+import StripeScreen from './screen'
+import {NavigationActions, StackActions} from 'react-navigation';
+const height = Math.round(Dimensions.get('window').height);
+const width = Math.round(Dimensions.get('window').height);
+
+class Stripe extends Component {
+
+  constructor(props) {
+    super(props);
+    this.webview = null;
+    this.state = {
+      isLoading: false,
+      paypalUrl: null,
+      cardDetails: null,
+      paymentIntent: null,
+      logos: [
+        require('assets/VisaColored.png'),
+        require('assets/MastercardColored.jpg'),
+        require('assets/AmericanExpressColored.png'),
+        require('assets/dinersClub.png'),
+        require('assets/discoverColored.png'),
+        require('assets/jcb.png'),
+        require('assets/unionpay.png')
+      ]
+    };
+  }
+
+  componentDidMount(){
+    this.createPaymentIntent() 
+  }
+
+  createPaymentIntent(){
+    const {params} = this.props.navigation.state;
+    const { user } = this.props.state;
+    if(params && params.data == null) return null
+    if(user == null) return null
+    this.setState({
+      isLoading: true
+    })
+    Api.request(Routes.createPaymentIntent, {
+      account_id: user.id,
+      amount: params.data.amount,
+      currency: params.data.currency,
+      charge: params.data.charge,
+      total: params.data.total,
+      email: user.email
+    }, response => {
+      this.setState({
+        isLoading: false
+      })
+      this.setState({
+        paymentIntent: response.data
+      })
+    }, error => {
+      this.setState({
+        isLoading: false
+      })
+      console.log({
+        error
+      })
+    })
+  }
+
+  
+  onConfirmPayment(){
+    const {params} = this.props.navigation.state;
+    const { user } = this.props.state;
+    const { paymentIntent } = this.state;
+    if(params && params.data == null) return null
+    if(user == null) return null
+    if(paymentIntent == null) return null
+    this.setState({
+      isLoading: true
+    })
+    Api.request(Routes.confirmPaymentIntent, {
+      payment_intent_id: paymentIntent.id,
+      account_id: user.id,
+      email: user.email,
+      currency: params.data.currency,
+      charge: params.data.charge,
+      total: params.data.total,
+      amount: params.data.amount,
+      to: user.id
+    }, response => {
+      this.setState({
+        isLoading: false
+      })
+      this.navigate()
+    }, error => {
+      this.setState({
+        isLoading: false
+      })
+      console.log({
+        error
+      })
+    })
+  }
+
+  navigate = () => {
+    const navigateAction = NavigationActions.navigate({
+      routeName: 'drawerStack',
+      action: StackActions.reset({
+        index: 0,
+        key: null,
+        actions: [
+            NavigationActions.navigate({routeName: 'Dashboard', params: {
+              initialRouteName: 'Dashboard',
+              index: 0
+            }}),
+        ]
+      })
+    });
+    this.props.navigation.dispatch(navigateAction);
+  }
+
+  render() {
+    const { isLoading, paymentIntent } = this.state;
+    const { params } = this.props.navigation.state;
+    const { theme } = this.props.state;
+    return (
+      <View style={{
+        height: '100%'
+      }}>
+        {isLoading ? <Spinner mode="overlay" /> : null}
+        <ScrollView style={{
+          padding: 20,
+          width: '100%'
+        }}>
+          
+          <View style={{
+            padding: 15
+          }}>
+            {(params && params.data && paymentIntent) && (
+              <StripeScreen
+                theme={theme}
+                params={params}
+                paymentIntent={paymentIntent}
+                onConfirmPayment={() => this.onConfirmPayment()}
+                onComplete ={(cardDetails) => {
+                  this.setState({
+                    cardDetails
+                  })
+                }}
+                navigation={this.props.navigation}
+              />
+            )}
+          </View>
+        </ScrollView>
+      </View>
+    )
+  }
+}
+
+const mapStateToProps = state => ({ state: state });
+
+export default connect(
+  mapStateToProps
+)(Stripe)
---
+++ Payments/GooglePay/screen.js
@@ -0,0 +1,119 @@
+import React, { useEffect } from 'react';
+import { StripeProvider, useGooglePay } from '@stripe/stripe-react-native';
+import { View, Text, Alert, Dimensions } from 'react-native'
+import Config from 'src/config.js';
+import Helper from 'common/Helper'
+import Color from 'common/Color';
+import Button from 'components/Form/Button';
+const height = Math.round(Dimensions.get('window').height);
+export default function App(props) {
+  const { theme, params } = props;
+  const { isGooglePaySupported, initGooglePay } = useGooglePay();
+  const { createGooglePayPaymentMethod, presentGooglePay } = useGooglePay();
+
+  const initializeGoogle = async () => {
+    const { error } = await initGooglePay({
+      testEnv: true,
+      merchantName: 'Widget Store',
+      countryCode: 'US',
+      billingAddressConfig: {
+        format: 'FULL',
+        isPhoneNumberRequired: true,
+        isRequired: false,
+      },
+      existingPaymentMethodRequired: false,
+      isEmailRequired: true,
+    });
+
+    if (error) {
+      Alert.alert(error.code, error.message);
+      return;
+    } else {
+      pay();
+    }
+    setInitialized(true);
+  }
+
+  const pay = async () => {
+    const clientSecret = await fetchPaymentIntentClientSecret();
+  
+    const { error } = await presentGooglePay({
+      clientSecret,
+      forSetupIntent: true,
+      currencyCode: 'USD',
+    });
+  
+    if (error) {
+      Alert.alert(error.code, error.message);
+      return;
+    }
+    Alert.alert('Success', 'The SetupIntent was confirmed successfully.');
+  };
+
+  useEffect(() => {
+    initializeGoogle()
+  }, []);
+
+  const renderAmount = (data) => {
+    return(
+      <View>
+        <Text style={{
+          marginTop: 20,
+          textAlign: 'center',
+          fontSize: 55,
+          color: Color.darkGray
+        }}>
+          {data?.amount}
+        </Text>
+        <Text style={{
+          marginBottom: 20,
+          textAlign: 'center',
+          color: Color.darkGray
+        }}>
+          {data?.currency}
+        </Text>
+      </View>
+    )
+  }
+
+  const renderFooter = () => {
+    return (
+      <View style={{
+        width: '100%',
+        padding: 20,
+        alignItems: 'center',
+        position: 'absolute',
+        bottom: 100
+      }}>
+        <Button
+          style={{
+            backgroundColor: theme ? theme.secondary : Color.secondary,
+            position: 'absolute',
+            width: '100%',
+            bottom: 10
+          }}
+          title={'Proceed'}
+          onClick={() => {
+            openPaymentSheet()
+          }} />
+      </View>
+    )
+  }
+
+  return (
+    <StripeProvider
+      publishableKey={Config.stripe.pk}
+      merchantIdentifier="merchant.com.PAYHIRAM"
+    >
+      <View style={{
+        width: '100%',
+        flex: 1,
+        height: height
+      }}>
+      {
+        (params && params.data) && renderAmount(params.data)
+      }
+      </View>
+    </StripeProvider>
+  );
+}
